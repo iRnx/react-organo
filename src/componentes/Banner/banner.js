14b73f6a007733aded60baa289582316f3e06fd4
Renan <renan.rg@hotmail.com.br>
2024-01-20 22:34:05 -0300

    first commit

---
+++ src/componentes/Banner/banner.js
@@ -0,0 +1,14 @@
+import './banner.css'
+
+function Banner(){
+
+    return (
+        
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner princípal da página do Organo" />
+        </header>
+    )
+
+}
+
+export default Banner
