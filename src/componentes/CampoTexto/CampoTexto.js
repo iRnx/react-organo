8d2976d47767cdaab1435974adff1f9dc8671552
Renan <renan.rg@hotmail.com.br>
2024-01-23 17:44:35 -0300

    Finalizando o formulario

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,11 +1,16 @@
 import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
+
+
+    const aoDigitar = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
     
     return (
         <div className="campo-texto">
             <label htmlFor="Nome">{ props.label }</label>
-            <input required={props.obrigatorio} type="text" placeholder={ props.placeholder } />
+            <input value={props.valor} onChange={aoDigitar} required={props.obrigatorio} type="text" placeholder={ props.placeholder } />
         </div>
         
     )
